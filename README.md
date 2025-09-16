27986a29a8199abeb68c76e783a8f5ee3746d974
Charlie.Lee <c050820021a@gmail.com>
2025-09-16 18:04:11 +0800

    init project, finish some of add keyword page

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # newnews-frontend
+The frontend of my US stock news aggregator
+
+## Pre-requisite
+- N/A
+
+## Functions
+- Add a keyword for searching
+- Use the search keyword to construct categories and concepts
+- Get all the news by keywords, categories and concepts, display them accordingly
+
+## TODO
+- [ ] Add Keyword Page (Homepage)
+  - [x] Allow user to add a keyword
+    - [x] Duplication handling
+    - [x] Show the list of keywords
+  - [ ] Allow user to delete a keyword
+  - [ ] Submit all the keywords
+  - [ ] UX
+    - [ ] Undo keyword deletion
+    - [ ] Vim bindings for navigating keyword list (too fancy?)
+- [ ] Categories and Concept Page
+- [ ] News Display Page
+- [ ] UI (General)
+  - [ ] Pick a font
+  - [ ] Icons
+  - [ ] Color
+
+## Non Product Targets
+- [ ] Deeper understanding on vanilla JS (No Packages, tho i might use bun to serve TBC)
+  - [ ] JS in Browser
+  - [ ] Deploy the pages securely
+- [ ] Learn JSDoc
+- [ ] No Build JS
+- [ ] Deeper understanding on CSS (Again, no tailwind or preprocessor, pure css)
---
+++ index.js
@@ -0,0 +1,62 @@
+const keywordList = new Set();
+const keywordContainer = document.querySelector("div.keyword-container");
+const addKeywordContainer = document.querySelector("div.add-keyword-container");
+const addKeywordButton = document.getElementById("add-keyword-button");
+const addKeywordTextbox = document.getElementById("add-keyword-textbox");
+
+function handleAddKeyword() {
+    const keyword = addKeywordTextbox.value;
+    if (!keyword) {
+        return;
+    }
+
+    if (keywordList.has(keyword)) {
+        return;
+    }
+    keywordList.add(keyword);
+
+    const node = document.createElement("div");
+    node.textContent = keyword;
+    node.className = "keyword";
+    keywordContainer.appendChild(node);
+
+    addKeywordTextbox.value = null;
+    addKeywordTextbox.focus();
+    addKeywordButton.setAttribute("disabled", "true");
+}
+
+addKeywordButton.addEventListener("click", handleAddKeyword);
+
+addKeywordTextbox.addEventListener("input", function () {
+    const keyword = addKeywordTextbox.value;
+    if (!keyword) {
+        addKeywordButton.setAttribute("disabled", "true");
+        return;
+    }
+    addKeywordContainer.querySelectorAll("div.error").forEach(n => n.remove());
+
+    if (keywordList.has(keyword)) {
+        addKeywordButton.setAttribute("disabled", "true");
+        const node = document.createElement("div");
+        node.textContent = "Keyword already added";
+        node.className = "error";
+        addKeywordContainer.appendChild(node);
+        return;
+    }
+
+    addKeywordButton.removeAttribute("disabled");
+});
+
+addKeywordTextbox.addEventListener("keydown", function (e) {
+    if (e.repeat) {
+        return;
+    }
+    if (e.key !== "Enter") {
+        return;
+    }
+    handleAddKeyword();
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    addKeywordTextbox.focus();
+})
